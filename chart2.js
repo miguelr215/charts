583114da30760a7b11abe1f404901c4dc25d72ca
miguelr215 <drexelramos@gmail.com>
2022-12-19 06:41:16 -0800

    update doughnut

--- chart2.js
+++ chart2.js
@@ -7,17 +7,26 @@ let doughnut =  new Chart(ctx2, {
             'Red',
             'Blue',
             'Yellow',
-            'Green'
+            'Green', 
+            'Purple'
           ],
           datasets: [{
             label: 'My First Dataset',
-            data: [300, 50, 100, 125],
+            data: [50, 300, 100, 125, 75],
             backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(255, 205, 86)',
-              'rgb(75, 192, 25)'
-            ],
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 25, 0.2)',
+              'rgba(153, 102, 255, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 25, 1)',
+              'rgba(153, 102, 255, 1)'
+          ],
             hoverOffset: 4
           }]
     }
